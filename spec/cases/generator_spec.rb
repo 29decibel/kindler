dfe9af8d69a4611af29c15b98442ad47a332e7ed
29decibel <mike.d.1984@gmail.com>
2012-02-20 15:46:20 +0800

    add more specs

--- spec/cases/generator_spec.rb
+++ spec/cases/generator_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
-describe "Mobi html file generator" do
+describe "Mobi book file generator" do
+
+	DIR_PREFIX = "kindler_generated_mobi_"
+
+	it "should have the title,author property" do
+		title = 'first-book'
+		author = 'mike'
+		book = Kindler::Book.new :title=>title,:author=>author,:debug=>true
+		book.title.should == title
+		book.author.should == author
+	end
+
+	it "should have the generated dir contains book infos" do
+		title = 'first-book'
+		author = 'mike'
+		book = Kindler::Book.new :title=>title,:author=>author,:debug=>true
+		book.generate
+		File.should be_exist("./#{DIR_PREFIX}#{title}")
+	end
+
+	it "should contains the contents.html and ncx file" do
+		title = 'first-book'
+		author = 'mike'
+		book = Kindler::Book.new :title=>title,:author=>author,:debug=>true
+		book.add_page :title=>'page1',:author=>'mike1',:content=>'this is the page 1',:wrap=>true
+		File.should be_exist("./#{DIR_PREFIX}#{title}/contents.html")
+		File.should be_exist("./#{DIR_PREFIX}#{title}/nav-contents.ncx")
+	end
+
+	it "contents file should include the page" do
+		title = 'first-book'
+		author = 'mike'
+		book = Kindler::Book.new :title=>title,:author=>author,:debug=>true
+		book.add_page :title=>'page1',:author=>'mike1',:content=>'this is the page 1',:wrap=>true
+		book.generate
+		contents = File.open("./#{DIR_PREFIX}#{title}/contents.html").readlines
+		contents.count.should > 0
+		contents.select {|a| a.include?("001.html")}.count.should > 0
+	end
 
 	it "should not generate a mobi book" do
 		title = 'good_book'
@@ -8,7 +46,7 @@ describe "Mobi html file generator" do
 		book.add_page :title=>'page2',:author=>'mike1',:content=>'this is the page 2',:wrap=>true
 		book.add_page :title=>'page3',:author=>'mike1',:content=>'this is the page 3',:wrap=>true
 		book.generate 
-		File.exist?(mobi_book_path(title)).should == true
+		File.should be_exist("./#{DIR_PREFIX}#{title}/#{title}.mobi")
 	end
 
 	it "should generate book contains images" do
@@ -18,41 +56,29 @@ describe "Mobi html file generator" do
 		book.add_page :title=>'page2',:author=>'mike1',:content=>'this is the page 2',:wrap=>true
 		book.add_page :title=>'page3',:author=>'mike1',:content=>'<img src="http://media2.glamour-sales.com.cn/media/catalog/category/Stroili_banner_02.jpg"></img>this is the page 3',:wrap=>true
 		book.generate 
-		File.exist?(mobi_book_path(title)).should == true
+		File.should be_exist("./#{DIR_PREFIX}#{title}/1.jpg")
 	end
 
-	it "should generate book with sections" do
-		title = 'first_section_book'
-		book = Kindler::Book.new :title=>title,:author=>'mike',:debug=>true
-		book.add_page :title=>'page1',:author=>'mike1',:content=>'this is the page 1',:wrap=>true,:section => 'love'
-		book.add_page :title=>'page2',:author=>'mike1',:content=>'this is the page 2',:wrap=>true,:section => 'hate'
-		book.add_page :title=>'page3',:author=>'mike1',:content=>'<img src="http://media2.glamour-sales.com.cn/media/catalog/category/Stroili_banner_02.jpg"></img>this is the page 3',:wrap=>true,:section=>'hate'
-		book.generate 
-		File.exist?(mobi_book_path(title)).should == true
-	end
-
-	it "should contains right info at contents html" do
+	it "can access pages information before generate" do
 		title = 'test_contents'
 		book = Kindler::Book.new :title=>title,:author=>'mike',:debug=>true
 		book.add_page :title=>'love page1',:author=>'mike1',:content=>'this is the love page1',:section => 'love'
 		book.add_page :title=>'love page2',:author=>'mike1',:content=>'this is the love page2',:section => 'love'
 		book.add_page :title=>'hate page1',:author=>'mike1',:content=>'this is the hate page1',:section => 'hate'
 		book.add_page :title=>'love page3',:author=>'mike1',:content=>'this is the love page3',:section => 'love'
-		book.add_page :title=>'hate page2',:author=>'mike1',:content=>'<img src="http://media2.glamour-sales.com.cn/media/catalog/category/Stroili_banner_02.jpg"></img>this is the hate page2',:section=>'hate'
+		book.pages.count.should == 4
+	end
+
+
+	it "should have two sections" do
+		title = 'test_contents'
+		book = Kindler::Book.new :title=>title,:author=>'mike',:debug=>true
+		book.add_page :title=>'love page1',:author=>'mike1',:content=>'this is the love page1',:section => 'love'
+		book.add_page :title=>'love page2',:author=>'mike1',:content=>'this is the love page2',:section => 'love'
+		book.add_page :title=>'hate page1',:author=>'mike1',:content=>'this is the hate page1',:section => 'hate'
+		book.add_page :title=>'love page3',:author=>'mike1',:content=>'this is the love page3',:section => 'love'
 		book.generate 
-		book.pages[2][:title].should == 'love page3'
-	end
-
-	it "should contains right info at ncx file" do
-		
-	end
-
-	it "should contains right info at opf file" do
-		
-	end
-
-	def mobi_book_path(title,output_dir='.')
-		File.join(output_dir,"kindler_generated_mobi_#{title}/#{title}.mobi")
+		book.pages_by_section.count.should == 2
 	end
 
 end
